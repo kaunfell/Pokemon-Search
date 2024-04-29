9f2f708ffc6087cc5674b0a1a9c01cd61773b350
kaunfell <jenny.kaunistunturi@gmail.com>
2024-04-29 13:20:15 +0300

    test 3

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ function App() {
 
   //async, this is like getting stuff from other website etc. The pokemon.json is in other file to represent that
   React.useEffect(() => {
-    fetch("http://localhost:3000/pokemon.json")
+    fetch("https://gist.githubusercontent.com/jherr/23ae3f96cf5ac341c98cd9aa164d2fe3/raw/f8d792f5b2cf97eaaf9f0c2119918f333e348823/pokemon.json")
     .then((resp) => resp.json())
   .then((data) => setPokemon(data))
   }, []);
