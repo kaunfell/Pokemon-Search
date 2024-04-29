b87e3bde60d89c5a3ef0f72c6fd7dad7e5811c18
kaunfell <jenny.kaunistunturi@gmail.com>
2024-04-29 10:34:27 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,106 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+//import pokemon from "./Pokemon.json";
+import PropTypes from 'prop-types';
+
+
+const PokemonRow = ({pokemon, onSelect}) => (
+  <tr >
+  <td>{pokemon.name.english}</td>
+  <td>{pokemon.type.join(", ")}</td>
+  <td>
+    <button
+      onClick={() => onSelect(pokemon)}
+      >Select</button>
+
+  </td>
+</tr>
+);
+//    <h1>{name.english}</h1>
+const PokemonInfo = ({name, base}) => (
+  <div className='test'>
+
+
+    <table className='test2'>
+    <h3>{name.english}</h3>
+      {
+        Object.keys(base).map((key) =>(
+          <tr key={key}>
+            <td>{key}</td>
+            <td>{base[key]}</td>
+          </tr>
+        ))
+      }
+    </table>
+  </div>
+)
+
+/*PokemonRow.PropTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.shape({
+      english: PropTypes.string,
+    }),
+    type: PropTypes.arrayOf(PropTypes.string),
+  }),
+};*/
 
 function App() {
+  const [filter, setFilter] = React.useState("");
+  const [pokemon, setPokemon] = React.useState([]);
+  const [selectedItem, setSelectedItem] = React.useState(null);
+  console.log(selectedItem)
+
+
+  //async, this is like getting stuff from other website etc. The pokemon.json is in other file to represent that
+  React.useEffect(() => {
+    fetch("http://localhost:3000/pokemon.json")
+    .then((resp) => resp.json())
+  .then((data) => setPokemon(data))
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+    > 
+      <div className='header'>
+        <h1>Pokemon Search</h1>
+        <input
+          value={filter}
+          onChange={(event) => setFilter(event.target.value)}
+          />
+      </div>
+
+      <div className='pokemonContainer'>
+      <table >
+        <thead >
+        <tr>
+            <th>Name</th>
+            <th>Type</th>
+          </tr>
+        </thead>
+
+        <tbody >
+ 
+          {pokemon
+          .filter((pokemon) => pokemon.name.english.toLowerCase().includes(filter.toLowerCase()))
+          .map(pokemon => (
+
+
+          < PokemonRow
+          pokemon={pokemon}
+          key={pokemon.id}
+          onSelect={(pokemon) => setSelectedItem(pokemon)} />
+          
+
+          ))}
+   
+        </tbody>
+
+
+        </table>
+        {selectedItem && <PokemonInfo {... selectedItem} />}
+      
+
+      </div>
     </div>
   );
 }
